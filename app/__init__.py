df16df6082a14e6bda0e121c7429b97b935c17f5
Shazman Malik <=>
2019-10-05 02:34:11 +0530

    Created Task

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify, send_file, Response
 from celery import Celery
 
 
 app = Flask(__name__)
 app.config['CELERY_IMPORTS'] = 'app.tasks'
-celery = Celery('task',broker='redis://localhost:6379', backend='redis://')
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379'
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379'
+app.config['CELERY_TASK_SERIALIZER'] = 'json'
+app.config['CELERY_ACCEPT_CONTENT'] = ['application/json']
+
+def make_celery(app):
+    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
+                    broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+celery = make_celery(app)
 
 from app import tasks
 
@@ -13,10 +31,12 @@ from app import tasks
 def index():
     return 'Welcome to Celery'
 
-@app.route('/task/<time>')
-def task(time):
-    task = tasks.example.delay(int(time))
-    return task.id
+@app.route('/extract')
+def extract():
+    task = tasks.extract_matches.delay()
+    dic = {}
+    dic['id'] = task.id
+    return jsonify(dic)
 
 @app.route('/task/status/<task_id>')
 def status(task_id):
@@ -31,7 +51,12 @@ def result(task_id):
     if task.state=='PENDING':
         return "Task is Being Executed"
     elif task.state=='SUCCESS':
-        return task.result
+        path = './app/files/'+ task.result
+        with open(path, 'r') as file:
+            return Response(file.read(),
+                            mimetype="json",
+                            headers={"Content-disposition":
+                            "attachment; filename="+task.result})
     else:
         return task.info
     return "Some Error"
--- app/tasks.py
+++ app/tasks.py
@@ -1,5 +1,10 @@
 import time
+from flask import jsonify
 from app import celery
+import csv
+import json
+import random
+import os
 
 
 @celery.task
@@ -7,3 +12,22 @@ def example(duration):
     time.sleep(duration)
     return 'Task Completed'
 
+@celery.task()
+def extract_matches():
+    matches_won_by_team = {}
+    seasons = ['2008', '2009', '2010', '2011', '2012', '2013','2014','2015','2016','2017']
+    with open('matches.csv') as matches_file:
+        matches = csv.DictReader(matches_file)
+        for match in matches:
+            winner = match['winner']
+            season = match['season']
+            if winner not in matches_won_by_team:
+                matches_won_by_team[winner] = {}
+                for each_season in seasons:
+                    matches_won_by_team[winner][each_season] = 0
+            matches_won_by_team[winner][season] += 1
+    filename = str(random.randint(99999, 999999)) + '.json'
+    filepath = './app/files/'+ filename
+    with open(filepath, 'w') as file:
+        json.dump(matches_won_by_team, file)
+    return filename
